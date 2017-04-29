735be45c5c73656c40d4748dbe22da87d3c82d5a
nazrul <nazruldiu111@gmail.com>
2017-04-29 12:39:51 +0600

    Datatable projec

---
+++ DatatableCRUD/Models/Extended/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatatableCRUD.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+    }
+
+    public class EmployeeMetadata
+    {
+
+        [Required (AllowEmptyStrings =false,ErrorMessage="Please Provide First Name")]
+        public string FirstName { get; set; }
+        [Required (AllowEmptyStrings=false, ErrorMessage="Please Provide Last Name")]
+        public string LastName { get; set; }
+    }
+}
